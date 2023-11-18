827cc55918abc9cddc6cdcfa91a9837be489c016
valkarreno <148592654+valkarreno@users.noreply.github.com>
2023-11-17 21:38:34 -0300

    agregando lo de la rama

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     SensorManager sensorManager;
     Sensor sensor;
@@ -19,8 +38,14 @@ public class MainActivity extends AppCompatActivity {
     TextView textView1;
     TextView textView2;
     TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    private LocationManager locationManager;
 
-    int cont = 0;
+    private int cont = 0;
+
+
+    //codigo de maps AIzaSyAbSKFzXrxbYOGhzqklIw3EWTlzT1f1GJo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +55,13 @@ public class MainActivity extends AppCompatActivity {
         textView1 = findViewById(R.id.textView1);
         textView2 = findViewById(R.id.textView2);
         textView3 = findViewById(R.id.textView3);
-
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
 
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if(sensor != null) {
+        if (sensor != null) {
             sensorEventListener = new SensorEventListener() {
                 @Override
                 public void onSensorChanged(SensorEvent sensorEvent) {
@@ -46,15 +72,20 @@ public class MainActivity extends AppCompatActivity {
                     textView2.setText("y: " + String.valueOf(y));
                     textView3.setText("z: " + String.valueOf(z));
 
-                    if(x < -4 && cont == 0){
+                    if (x < -4 && cont == 0) {
                         cont++;
                     }
-                    if(x > 4 && cont == 1){
+                    if (x > 4 && cont == 1) {
                         cont++;
                     }
-                    if(cont == 2){
+                    if (cont == 2) {
                         latigo();
                         cont = 0;
+                    }
+                    //Runtime permissions
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
                     }
                 }
 
@@ -67,16 +98,63 @@ public class MainActivity extends AppCompatActivity {
         start();
     }
 
-    private void start(){
+    private void start() {
         sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
-    private void stop(){
+
+    private void stop() {
         sensorManager.unregisterListener(sensorEventListener);
     }
 
-    private void latigo(){
+    private void latigo() {
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.latigo);
         mediaPlayer.start();
     }
 
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void ubicacion(View view) {
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location != null){
+            textView4.setText("Latidud: " + String.valueOf(location.getAltitude()));
+            textView5.setText("Longitud: " + String.valueOf(location.getLongitude()));
+        }
+
+    }
 }
